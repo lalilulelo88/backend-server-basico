3aed564efe24da3fd35a455b539b7508c858dcbe
Luis Osorio <losorio@consejodeauditoria.gob.cl>
2019-11-19 17:47:20 -0300

    Feat: CRUD usuario backend

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
-
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+// Body Parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// importar Rutas
+
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexion a BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) =>{
@@ -18,16 +28,12 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, resp) 
 
 
 // Rutas
-app.get('/', (req, res, next)=>{
-
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+app.use('/usuario',usuarioRoutes);
+app.use('/login',loginRoutes);
+app.use('/',appRoutes);
 
 
 //Escuchar peticiones
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online');
 });
---
+++ config/config.js
@@ -0,0 +1,1 @@
+module.exports.SEED = '@seed@ejemplo@test';
